aa436b72d10b576dcc226198fca436428959c2f5
Pietro <pietrobds651@gmail.com>
2024-11-10 23:40:26 -0300

    refactor: trocando 'from .' para 'from .jogodavelha'

---
+++ jogovelha/__init__.py
@@ -0,0 +1,7 @@
+#importando como .jogodavelha para chamar direto por "jogovelha"
+
+from .jogodavelha import JogoVelha
+from .jogodavelha import Tabuleiro
+from .jogodavelha import Jogador
+from .jogodavelha import JogadorHumano
+from .jogodavelha import JogadorComputador
